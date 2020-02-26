084ffc78ae909e2470d9802389f70513355aca8a
sepehr <sepehr@cerenade.com>
2020-02-26 08:51:36 -0800

    added nav-bar-search directive

---
+++ App/FlashCards.App/app/src/directives/fc-nav-bar-search.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('fcNavBarSearch', fcNavBarSearch);
+
+    function fcNavBarSearch() {
+        return {
+            restrict: 'E',
+            templateUrl: 'app/src/Directives/fc-nav-bar-search.html',
+            controller: function ($scope, $location) {
+                var path = $location.path()
+                if (path.includes('search')) {
+                    var index = path.lastIndexOf('/');
+                    $scope.searchPhrase = path.substr(index + 1);
+                }
+
+                $scope.search = function () {
+                    $scope.$broadcast('searchEvent', { searchPhrase: $scope.searchPhrase });
+                }
+                $scope.searchGlobal = function () {
+                    if ($scope.searchPhrase) {
+                        $location.path('/search/' + $scope.searchPhrase);
+                    }
+                }
+            }
+        };
+    }
+
+})();
--- App/FlashCards.App/app/src/directives/fc-nav-bar.js
+++ App/FlashCards.App/app/src/directives/fc-nav-bar.js
@@ -9,22 +9,6 @@
         return {
             restrict: 'E',
             templateUrl: 'app/src/Directives/fc-nav-bar.html',
-            controller: function ($scope, $location) {
-                var path = $location.path()
-                if (path.includes('search')) {
-                    var index = path.lastIndexOf('/');
-                    $scope.searchPhrase = path.substr(index + 1);
-                }
-
-                $scope.search = function () {
-                    $scope.$broadcast('searchEvent', { searchPhrase: $scope.searchPhrase });
-                }
-                $scope.searchGlobal = function () {
-                    if ($scope.searchPhrase) {
-                        $location.path('/search/' + $scope.searchPhrase);
-                    }
-                }
-            }
         };
     }
 
